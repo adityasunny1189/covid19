ec1b7208fdfad280bb6c04143d613cf44d29fbf9
adityasunny1189 <adityapathak1189@gmail.com>
2020-05-12 07:21:13 +0530

    version 1.0

---
+++ build/precache-manifest.12af1e005e05c43e52c5af7b6e47953e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4acb75c4b9b98dcbea3bacc265802ce2",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "80759c537b090562f417",
+    "url": "/covid19/static/css/main.1452688b.chunk.css"
+  },
+  {
+    "revision": "72f35cda944354057d53",
+    "url": "/covid19/static/js/2.902b260f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19/static/js/2.902b260f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80759c537b090562f417",
+    "url": "/covid19/static/js/main.7eb4ff00.chunk.js"
+  },
+  {
+    "revision": "aac322da9a7d6d61f03c",
+    "url": "/covid19/static/js/runtime-main.47c86a5b.js"
+  }
+]);
